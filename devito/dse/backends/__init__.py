6fb75a9ebf2cc59d2547814c080379b6a47755c4
George Bisbas <g.bisbas18@imperial.ac.uk>
2019-05-14 16:20:54 +0100

    remove file __init

--- devito/dse/backends/__init__.py
+++
@@ -1,8 +0,0 @@
-from devito.dse.backends.common import *  # noqa
-from devito.dse.backends.basic import BasicRewriter  # noqa
-from devito.dse.backends.advanced import (AdvancedRewriter, SkewingRewriter)  # noqa
-from devito.dse.backends.speculative import (SpeculativeRewriter,  # noqa
-                                             AggressiveRewriter,
-                                             CustomRewriter)
-
-# SkewingRewriter
